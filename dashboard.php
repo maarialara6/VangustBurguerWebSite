e71ceab0c961ef4f404d927744074ed8629c04ba
maarialara6 <eduardalara5921@gmail.com>
2026-08-14 19:52:13 -0300

    first commit

---
+++ dashboard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>VANGUST - Burguer</title>
+</head>
+<body>
+	<!-- Cabeçalho -->
+	<header class="topbar">
+		<div  class="brand">
+			<img src="" alt="Logo do restaurante">
+		</div>
+
+		<div class="user-info">
+			<span>Olá <strong>Adm</strong></span>
+		</div>
+	</header>
+
+</body>
+</html>
