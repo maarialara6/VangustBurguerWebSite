18fc6d7a40b36df8ce76f39d1f8e6f22a5a12668
maarialara6 <eduardalara5921@gmail.com>
2026-08-14 19:58:36 -0300

    first commit

--- dashboard.php
+++ dashboard.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>VANGUST - Burguer</title>
+	<title>VANGUST - Painel Geral</title>
 </head>
 <body>
 	<!-- Cabeçalho -->
@@ -14,8 +14,18 @@
 
 		<div class="user-info">
 			<span>Olá <strong>Adm</strong></span>
+			<a href="index.html" class="btn-sair">Sair</a>
 		</div>
 	</header>
+
+	<!-- Cabeçalho -->
+	<div>
+		<aside class="sidebar">
+			<nav class="nav-menu">
+				<a href=""></a>
+			</nav>
+		</aside>
+	</div>
 
 </body>
 </html>
