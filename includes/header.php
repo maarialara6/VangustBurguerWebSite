f951d617eacf684c228bc1f53eaf10b3fb9a5197
maarialara6 <eduardalara5921@gmail.com>
2026-08-14 21:24:55 -0300

    first commit

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<div>
+		<img src="" alt="Logo do restaurante">
+		<p>Painel do restaurante</p>
+	</div>
+
+	<div>
+		<form action="pedidos.php" method="GET">
+			<label></label>
+			<label></label>
+			<label></label>
+		</form>
+	</div>
+</body>
+</html>
