398f9465ed1c6a011a7315fbd753f113ca592148
maarialara6 <eduardalara5921@gmail.com>
2026-08-14 21:10:32 -0300

    first commit

--- dashboard.php
+++ dashboard.php
@@ -6,26 +6,6 @@
 	<title>VANGUST - Painel Geral</title>
 </head>
 <body>
-	<!-- Cabeçalho -->
-	<header class="topbar">
-		<div  class="brand">
-			<img src="" alt="Logo do restaurante">
-		</div>
-
-		<div class="user-info">
-			<span>Olá <strong>Adm</strong></span>
-			<a href="index.php" class="btn-sair">Sair</a>
-		</div>
-	</header>
-
-	<!-- Menu lateral -->
-	<div>
-		<aside class="sidebar">
-			<nav class="nav-menu">
-				<a href=""></a>
-			</nav>
-		</aside>
-	</div>
 
 </body>
 </html>
